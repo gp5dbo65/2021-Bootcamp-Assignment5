deea1a9653043cc9f9b8a1355ee17196aea5171d
gp5dbo65 <daveotis65@gmail.com>
2021-06-30 11:08:20 -0400

    Recommend code changes after Nathan's code review

--- src/com/coderscampus/arraylist/Assignment5App.java
+++ src/com/coderscampus/arraylist/Assignment5App.java
@@ -1,22 +1,25 @@
 package com.coderscampus.arraylist;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Random;
 
 public class Assignment5App {
 	public static final Integer UPPER_BOUND = 1001;
 
 	public static void main(String[] args) {
-		CustomList<Integer> numbers = new CustomArrayList<>();
+		CustomList<Integer> numbersList = new CustomArrayList<>();
 		Random random = new Random(); //generate a range of integers: 0-1000
 
 		/* Load CustomList<Integer>*/
 		for (int i = 0; i < 101; i++) {
-			numbers.add(random.nextInt(UPPER_BOUND)); //add 100 Integer in the CustomArrayList
+			numbersList.add(random.nextInt(UPPER_BOUND)); //add 100 Integer in the CustomArrayList
 		}
 		
 		System.out.println("Display contents of the Custom List");	
-		for (int i = 0; i < numbers.getSize(); i++) {
-			System.out.println("numbers["+ i +"] = " + numbers.get(i));
+		for (int i = 0; i < numbersList.getSize(); i++) {
+			System.out.println("numbers["+ i +"] = " + numbersList.get(i));
 		}
 		System.out.println("------------");
 		
@@ -39,7 +42,36 @@ public class Assignment5App {
 			System.out.println("randomStrings["+ i +"] = " + randomStrings.get(i));
 		}
 		System.out.println("------------");
+		
+		speedTest();
 
 	} //end of main
 
+	public static void speedTest() {
+		Date startTime = new Date();
+		
+		List<Integer> arrayListNumbers = new ArrayList<>();
+		
+		for (int i = 0; i < 100000000; i++) {
+			arrayListNumbers.add(i);
+		}
+		
+		Date endTime = new Date();
+		System.out.println("ARRAYLIST: The time it took to add numbers is: "
+			+ (endTime.getTime() - startTime.getTime()) + " milliseconds");
+		
+		Date startTimeCustomList = new Date();
+		
+		CustomList<Integer> customListNumbers = new CustomArrayList<>();
+		
+		for (int i = 0; i < 100000000; i++) {
+			customListNumbers.add(i);
+		}
+		
+		Date endTimeCustomList = new Date();
+		System.out.println("CUSTOM ARRAY LIST: The time it took to add numbers is: "
+			+ (endTimeCustomList.getTime() - startTimeCustomList.getTime()) + " milliseconds");
+		
+	} //end of speedTest method
+	
 } //end of Assignment5App class
